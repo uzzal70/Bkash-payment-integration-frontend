b44eb5d1fc97b3faab6adc0c04e52182940c9b8f
Md Uzzal Hossain <uzzalcse70@gmail.com>
2023-10-30 17:35:35 +0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/paywithbkash" element={<PayWithBkash />} />
-          <Route path="/api/payment/:message" element={<Response />} />
+          <Route path="/payment/:message" element={<Response />} />
         </Routes>
       </BrowserRouter>
     </div>
